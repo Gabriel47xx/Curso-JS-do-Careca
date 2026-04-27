3fbe5a34f79804467f99b1d00ccfa0b4684bab83
Gabriel Rezende Fernandes de Miranda <gabrielrezende565@gmail.com>
2026-04-27 15:42:06 -0300

    Add a funcionalidade de abrir os videos em Iframe

--- aula027ListaVideos/script.js
+++ aula027ListaVideos/script.js
@@ -30,6 +30,32 @@ function show(indice, indiceLista) {
 
 }
 
+// Lógica para abrir os vídeos no iframe
+const videoLinks = document.querySelectorAll('.videos a');
+const iframeVideo = document.getElementById('iframeVideo');
 
-
-
+videoLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // Impede que o link abra em uma nova aba
+        
+        const url = this.href;
+        let videoId = '';
+        
+        // Extrai o ID do vídeo da URL (funciona para youtu.be e youtube.com/watch)
+        if (url.includes('youtu.be/')) {
+            videoId = url.split('youtu.be/')[1].split('?')[0];
+        } else if (url.includes('youtube.com/watch')) {
+            const urlObj = new URL(url);
+            videoId = urlObj.searchParams.get('v');
+        }
+        
+        // Atualiza o src do iframeVideo e define as dimensões para exibir o vídeo
+        if (videoId) {
+            iframeVideo.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+            iframeVideo.width = '560';
+            iframeVideo.height = '315';
+            iframeVideo.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+            iframeVideo.allowFullscreen = true;
+        }
+    });
+});
